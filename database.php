732ba81beb6e917e26cb2fa8796bdad2bb0d3032
CelineLow <49967687+CelineLow@users.noreply.github.com>
2019-12-21 09:50:29 +0800

    Create database.php

---
+++ database.php
@@ -0,0 +1,11 @@
+<?php
+
+$host = "localhost";
+$username = "w0207";
+$password = "EeTQgPwCtR3UqaSn";
+$database = "w0207";
+
+
+$link = mysqli_connect($host,$username,$password,$database) or die (mysqli_error($link));
+
+?>
