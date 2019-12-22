ec32647adcf256d8d0922a3d77161f58e40bfbfc
CelineLow <49967687+CelineLow@users.noreply.github.com>
2019-12-22 21:07:16 +0800

    product list

--- CI/application/controllers/Frontend.php
+++ CI/application/controllers/Frontend.php
@@ -13,18 +13,17 @@ class Frontend extends CI_Controller{
         $data = [];
         $produc_list = [
             ["id" => 1, "title" => "TV"],
-            ["id" => 2, "title" => "SS"],
+            ["id" => 2, "title" => "Cooker"],
         ];
 
-        $data[] = $produc_list;
+        $data['produc_list'] = $produc_list;
 
         $this->load->view("header");
         $this->load->view("product_list", $data);
         $this->load->view("footer");
     }
 
-    public function product_detail($product_id){
-        $product_id = 1;
+    public function product_detail($product_id){        
         echo"<h1>product detail".$product_id."</h1>";
     }
 }
--- CI/application/views/product_list.php
+++ CI/application/views/product_list.php
@@ -2,8 +2,8 @@
 
 <?php
 
-// foreach($produc_list as ($v)){
-
-// }
+foreach($produc_list as $v){
+    echo $v['id']." <a href='".base_url('product_detail/'.$v['id'])."'>".$v['title']."</a><br/>";
+}
 
 ?>
